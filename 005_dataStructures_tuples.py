c50092a00c7a4bbfe0733fc41ad1b399c751c2d8
Amirali_Ghofrani <amiralighofrani1999@protonmail.com>
2025-11-12 23:14:54 +0330

    tuples update

--- 005_dataStructures_tuples.py
+++ 005_dataStructures_tuples.py
@@ -7,3 +7,9 @@ my_tuple = (0, 1, 2, 3, "Tehran", "Boston", 3.14)
 
 print (my_tuple[4])                   # prints Tehran
 
+# problem: how to define distance between two points:
+coordinates = (3,4)
+x2 = int(input("x2 = "))
+y2 = int(input("y2 = "))
+distance = ((x2 - coordinates[0]) **2 + (y2 - coordinates[1]) **2 ) ** 0.5
+print("distance = ", distance)
