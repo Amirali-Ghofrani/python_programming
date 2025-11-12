fa1e66ef62b1531ee82338bb523cfa6ed8e9eebf
Amirali_Ghofrani <amiralighofrani1999@protonmail.com>
2025-11-12 17:01:34 +0330

    how to define and work with dictionaries

---
+++ 004_dataStructures_dictionaries.py
@@ -0,0 +1,36 @@
+# how to define a dictionary:
+score = {"math" : 90, "programming" : 100, "science" : 80}
+print(score)
+print(score["math"])             # prints 90
+
+# a dictionary can be defined into another dictionary as well:
+
+score_total = {
+           
+           "score_sara" :{
+               "math" : 90, 
+               "programming" : 100, 
+               "science" : 80
+           },
+
+           "score_amirali":{
+               "math" : 100,
+               "programming": 95,
+               "physics": 80
+           }
+}
+
+print(score_total["score_sara"]["math"])
+print (score_total["score_amirali"]["programming"])
+
+# how to return keys in a dictionary:
+keys = score.keys()
+print(keys, type(keys)) 
+
+# how to return values in a dictionary:
+values = score.values()
+print(values, type(values)) 
+
+# how to return keys and values:
+items = score.items()
+print(items, type(items))
