f91c773a0a586e18d44a16ca102efce570894e06
Amirali_Ghofrani <amiralighofrani1999@protonmail.com>
2025-11-12 23:03:14 +0330

    how to define and work with tuples

---
+++ 005_dataStructures_tuples.py
@@ -0,0 +1,9 @@
+# tuples are ordered datastructures similar to lists, but immutable
+# how to define a tuple:
+
+my_tuple = (0, 1, 2, 3, "Tehran", "Boston", 3.14)
+
+# other methods used in lists are appliable for tuples as well(except value assignment)
+
+print (my_tuple[4])                   # prints Tehran
+
