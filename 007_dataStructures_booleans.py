72e71194d0988b4eef1efcc63e273b06a97bba39
Amirali_Ghofrani <amiralighofrani1999@protonmail.com>
2025-11-13 15:10:28 +0330

    how to define and work with booleans in python

---
+++ 007_dataStructures_booleans.py
@@ -0,0 +1,10 @@
+my_bool0 = True
+my_bool1 = None
+
+print(my_bool0, type(my_bool0))
+print (my_bool1, type(my_bool1))
+
+# to check if a list contains a certain value and return a boolean type: 
+my_list = ["panda", "monkey", "dog", "horse", "parrot"]
+my_bool2 = "panda" in my_list
+print(my_bool2)
