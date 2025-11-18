1fafb503557e4b56c7793b45eadc9669c2703f20
Amirali_Ghofrani <amiralighofrani1999@protonmail.com>
2025-11-18 22:48:35 +0330

    update - some point on function parameters

--- 015_functions.py
+++ 015_functions.py
@@ -10,9 +10,9 @@ def askName():               # gets name of the user and returns it as a string
     return name
 
 
-def helloName(name, n):      # gets a string and an int as parameters, prints the given string for n times 
-    for i in range (n):
-        print(f"Hello dear {name}!")
+def helloName(name, n = 1):           # gets a string and an int as parameters, prints the given string for n times. also
+    for i in range (n):               # the function sets the default value for n to 1, unless it is given another value when 
+        print(f"Hello dear {name}!")  # the function is called
     return 0
 
 # it's better to define a main function and call other functions inside the main(c++ style!)
@@ -20,9 +20,12 @@ def main():
 
     helloWorld()        # calling helloWorld function inside the main
     name = askName()
-    helloName(name, 5)
+    helloName(name, 5)  # also can be written as helloName(name, n = 5)
 
     return 0
 
 
 main()              # calling the main function
+
+# function parameters are dynamic type in python, so they can deffer in type(int,
+# string,...) each time the function is called
