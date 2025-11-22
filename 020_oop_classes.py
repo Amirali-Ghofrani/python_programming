3914e393616f62ec7fed027c9bede21618226439
Amirali_Ghofrani <amiralighofrani1999@protonmail.com>
2025-11-22 23:57:03 +0330

    update

--- 020_oop_classes.py
+++ 020_oop_classes.py
@@ -1,8 +1,10 @@
 # how to define a class:
 # creating an object called Book:
+# note: 'self' is not a key word and it's just used as a conventional word, it refers to the object
+# without using self, our variables would be local and not the object attributes
 
 class Book():
-    def getInfo(self, n,p):
+    def getInfo(self, n,p): 
         self.name = n
         self.pages = p
 
@@ -15,16 +17,20 @@ my_book.printInfo()
 
 
 
-# we can use a cunstructor to be called automatically whenever an object is created:
+# we can use a cunstructor to get called automatically whenever an object is created:
 class Car():
-    def __init__(self, model, color):
-        self.model = model
+    def __init__(self, m = "unknown", color = "unknown"):
+        self.model = m
         self.color = color
 
 # now, when an object from class car is created, the init function will run automatically
 # (we don't need to call it manually):
+# note: we can't have multiple constructors like in cpp, we can use default values instead
 
 my_car = Car("Toyota", "Black")
 print(my_car.model, my_car.color)
+
+my_car2 = Car()
+print(my_car2.model, my_car2.color)
 
     
